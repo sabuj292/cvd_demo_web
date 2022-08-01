138c6f3ace73792775349185d9c4c739365933e5
sabuj292 <64396430+sabuj292@users.noreply.github.com>
2022-08-01 12:28:01 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
+from collections.abc import Mapping
 #from requests import request
 
 # app= Flask(__name__, template_folder='Template')
